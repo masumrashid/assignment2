f1164ae6d575f78f687a63fe513aad03f6670261
masumrashid <razzshikder@gmail.com>
2025-02-05 23:51:03 +0600

    new

--- lib/main.dart
+++ lib/main.dart
@@ -21,28 +21,35 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController name = TextEditingController();
+    TextEditingController age = TextEditingController();
+    TextEditingController salary = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Employe'),
       ),
-      body: Form(
-          child: Column(
+      body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(15.0),
             child: TextFormField(
+              controller: name,
               decoration: InputDecoration(labelText: "Name", hintText: "Name"),
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(15.0),
             child: TextFormField(
+              controller: age,
+              keyboardType: TextInputType.number,
               decoration: InputDecoration(labelText: "Age", hintText: "Age"),
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(15.0),
             child: TextFormField(
+              controller: salary,
+              keyboardType: TextInputType.number,
               decoration:
                   InputDecoration(labelText: "Salary", hintText: "Salary"),
             ),
@@ -51,7 +58,7 @@ class Home extends StatelessWidget {
               child:
                   ElevatedButton(onPressed: () {}, child: Text("Add Employee")))
         ],
-      )),
+      ),
     );
   }
 }
